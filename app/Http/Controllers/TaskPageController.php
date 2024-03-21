12c9e39fcc003a950b6b55b463dc4fb847024a3d
nikoladomazetovikj <nikolad150@gmail.com>
2024-03-21 18:18:06 +0100

    fix: protect edit task

--- app/Http/Controllers/TaskPageController.php
+++ app/Http/Controllers/TaskPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Task\EditTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -29,7 +30,7 @@ class TaskPageController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Task $task): \Inertia\Response
+    public function edit(Task $task, EditTaskRequest $request): \Inertia\Response
     {
         return Inertia::render('Task/EditTask', ['task' => $task]);
     }
